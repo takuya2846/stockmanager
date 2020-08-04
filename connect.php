5542525d392bb0148cd1334bc9259c5ccd775e8e
taku2846 <hpvmn7059duj@gmail.com>
2020-08-04 15:06:07 +0900

    connect.phpの変更

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php
-  $username = 'root';
-  $password = 'root';
-
+  $username = 'b1e6180fcbc2da';
+  $password = 'fbcab897';
+  $dbname = 'heroku_8003064f452ae0f';
+  $host = 'us-cdbr-east-02.cleardb.com';
   try {
-    $db = new PDO('mysql:dbname=stocksdb;host=localhost;charset=utf8',$username,$password);
+    $db = new PDO('mysql:dbname=$dbname;host=$host;charset=utf8',$username,$password);
   } catch (PDOException $e) {
     echo 'DB接続エラー：' . $e-> getMessage();
   }
